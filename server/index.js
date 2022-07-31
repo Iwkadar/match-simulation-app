4f3e43b2cc06e19ff95b99256ea0e04c30f4bfe0
Iwona <iwadar90@gmail.com>
2022-07-31 18:51:35 +0200

    handled get request on the server

--- server/index.js
+++ server/index.js
@@ -5,8 +5,17 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get('/api', (req, res) => {
-    res.json({ message: 'Hello from server!' });
+const teams = {
+    0: { id: 0, name: 'Poland', score: 0 },
+    1: { id: 1, name: 'Germany', score: 0 },
+    2: { id: 2, name: 'Brazil', score: 0 },
+    3: { id: 3, name: 'Mexico', score: 0 },
+    4: { id: 4, name: 'Argentina', score: 0 },
+    5: { id: 5, name: 'Uruguay', score: 0 }
+};
+
+app.get('/teams', (req, res) => {
+    res.json(teams);
 });
 
 app.use(express.static(path.resolve(__dirname, '../dist')));
