02999d004b62c42ba0552708b78835e5a9b89847
Iwona <iwadar90@gmail.com>
2022-08-01 20:52:47 +0200

    fixed store types

--- src/types/app.ts
+++ src/types/app.ts
@@ -1,3 +1,5 @@
-export type RootState = ReturnType<typeof import('../store').store.getState>;
-export type AppDispatch = typeof import('../store').store.dispatch;
+// export type RootState = ReturnType<typeof import('../store').store.getState>;
+// export type AppDispatch = typeof import('../store').store.dispatch;
+export type RootState = ReturnType<typeof import('../store').rootReducer>;
 export type AppStore = ReturnType<typeof import('../store').setupStore>;
+export type AppDispatch = AppStore['dispatch'];
